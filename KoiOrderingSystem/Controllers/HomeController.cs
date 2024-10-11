691829ed1f563e185ae9d19843a0508cea274ddf
Phan Anh Khoa <phananhkhoa456@gmail.com>
2024-10-11 10:40:36 +0700

    Cap nhat controller

--- KoiOrderingSystem/Controllers/HomeController.cs
+++ KoiOrderingSystem/Controllers/HomeController.cs
@@ -42,5 +42,9 @@ namespace KoiOrderingSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult FishBreeds()
+        {
+            return View();
+        }
     }
 }
