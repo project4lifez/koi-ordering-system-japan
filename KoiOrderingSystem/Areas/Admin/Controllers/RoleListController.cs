d958172ca09e38e9112f357b5af29f1367d6a000
HoangViet204 <vietHQSE183402@fpt.edu.vn>
2024-10-23 22:49:32 +0700

    CRUD cho Role

--- KoiOrderingSystem/Areas/Admin/Controllers/RoleListController.cs
+++ KoiOrderingSystem/Areas/Admin/Controllers/RoleListController.cs
@@ -1,4 +1,5 @@
 ﻿using KoiOrderingSystem.Controllers.Admin;
+using KoiOrderingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KoiOrderingSystem.Areas.Admin.Controllers
@@ -7,19 +8,91 @@ namespace KoiOrderingSystem.Areas.Admin.Controllers
 
     public class RoleListController : BaseController
     {
+        private readonly Koi88Context _db;
 
+        public RoleListController(Koi88Context db)
+        {
+            _db = db;
+        }
         public IActionResult RoleList()
         {
-            return View();
+            
+            var roles = _db.Roles.ToList();
+            return View(roles); 
         }
 
+        [HttpGet]
         public IActionResult CreateRole()
         {
             return View();
         }
-        public IActionResult UpdateRole()
+
+        [HttpPost]
+        public IActionResult CreateRole(string RoleName)
         {
-            return View();
+            if (string.IsNullOrEmpty(RoleName))
+            {
+                ModelState.AddModelError("", "Role name is required.");
+                return View();
+            }
+
+            // Create a new role instance and save it to the database
+            var newRole = new Role
+            {
+                Name = RoleName
+            };
+
+            _db.Roles.Add(newRole);
+            _db.SaveChanges();
+
+            return RedirectToAction("RoleList","Admin");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteRole(int id)
+        {
+            // Find the role by ID
+            var role = _db.Roles.Find(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            _db.Roles.Remove(role);
+            _db.SaveChanges();
+
+            return RedirectToAction("RoleList", "Admin");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateRole(int id)
+        {
+            var role = _db.Roles.Find(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+
+        // POST: /Admin/RoleList/UpdateRole
+        [HttpPost]
+        public IActionResult UpdateRole(Role updatedRole)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(updatedRole);
+            }
+
+            var existingRole = _db.Roles.Find(updatedRole.RoleId);
+            if (existingRole == null)
+            {
+                return NotFound();
+            }
+
+            existingRole.Name = updatedRole.Name;
+            _db.SaveChanges();
+            return Redirect($"/Admin/RoleList/UpdateRole/{updatedRole.RoleId}");
         }
     }
 }
