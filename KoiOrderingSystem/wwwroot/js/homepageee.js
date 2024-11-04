ede358bc38865137bfa6ab805eae9c100e7a06f0
Le Minh Thuan <thuanlmse183479@fpt.edu.vn>
2024-11-05 01:55:16 +0700

    update image for variety details

--- KoiOrderingSystem/wwwroot/js/homepageee.js
+++ KoiOrderingSystem/wwwroot/js/homepageee.js
@@ -9986,7 +9986,7 @@ function truncateToTwoLines(text) {
 // Function to show the full text when "Detail" is clicked
 function showFullText(button) {
     const description = button.previousElementSibling; // Get the description element
-    const fullText = description.getAttribute('data-full-text'); // Get the full text
+    const fullText = description.getAttribute('data-full-text'); 
     description.innerText = fullText; 
 
     // Retrieve the FarmId from the button's data attribute
