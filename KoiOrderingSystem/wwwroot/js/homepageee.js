da1aa5d42ddd12e050dbd1e7d3395fc95c6f1624
Nguyen Thanh Tai <taintse183488@fpt.edu.vn>
2024-11-05 01:26:40 +0700

    Redesign view of consulting staff dashboard

--- KoiOrderingSystem/wwwroot/js/homepageee.js
+++ KoiOrderingSystem/wwwroot/js/homepageee.js
@@ -47614,14 +47614,7 @@ if (filterBtns.length && varietyCards.length) {
     });
 }
 
-// Function to truncate text to two lines
-function truncateText() {
-    const descriptions = document.querySelectorAll('.farm-description');
-    descriptions.forEach(description => {
-        const fullText = description.getAttribute('data-full-text');
-        description.innerText = truncateToTwoLines(fullText);
-    });
-}
+
 
 
 
