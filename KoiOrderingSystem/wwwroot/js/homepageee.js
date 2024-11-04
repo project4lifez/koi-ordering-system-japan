73ea9e2959f38cc5b3a65e6986634a56336e6e73
Le Minh Thuan <thuanlmse183479@fpt.edu.vn>
2024-11-05 01:30:55 +0700

    update function order management v2

--- KoiOrderingSystem/wwwroot/js/homepageee.js
+++ KoiOrderingSystem/wwwroot/js/homepageee.js
@@ -8618,8 +8618,8 @@ function truncateToTwoLines(text) {
 
 // Function to show the full text when "Detail" is clicked
 function showFullText(button) {
-    const description = button.previousElementSibling; // Get the description element
-    const fullText = description.getAttribute('data-full-text'); // Get the full text
+    const description = button.previousElementSibling; 
+    const fullText = description.getAttribute('data-full-text'); 
     description.innerText = fullText; // Display the full text
 
     // Retrieve the FarmId from the button's data attribute
