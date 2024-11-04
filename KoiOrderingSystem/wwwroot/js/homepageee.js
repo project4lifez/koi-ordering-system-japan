a3604fdaf6bd05a064b60859870f6ac8e4c23edd
Le Minh Thuan <thuanlmse183479@fpt.edu.vn>
2024-11-05 01:27:51 +0700

    update logic function for consulting staff v2

--- KoiOrderingSystem/wwwroot/js/homepageee.js
+++ KoiOrderingSystem/wwwroot/js/homepageee.js
@@ -8691,7 +8691,7 @@ function truncateToTwoLines(text) {
 // Function to show the full text when "Detail" is clicked
 function showFullText(button) {
     const description = button.previousElementSibling; 
-    const fullText = description.getAttribute('data-full-text'); // Get the full text
+    const fullText = description.getAttribute('data-full-text'); 
     description.innerText = fullText; 
 
     // Retrieve the FarmId from the button's data attribute
